5219e510240b31013740f74166f880f28d01d37a
Darth Vader <omanlegend4@gmail.com>
2026-05-09 19:35:42 +0300

    feat: add component tests, all 14 passing

--- src/App.jsx
+++ src/App.jsx
@@ -3,12 +3,17 @@ import About from "./components/About.jsx";
 import ArticleList from "./components/ArticleList.jsx";
 import logo from "./assets/logo.png"
 
+const articles = [
+    { id: 1, title: "My First Article", date: "2024-06-01", preview: "First article" },
+    { id: 2, title: "My Second Article", date: "2024-06-02", preview: "Second article" }
+];
+
 function App() {
   return (
     <div className="App">
       <Header name="My Blog"/>
       <About image={logo} about="the blog text"/>
-      <ArticleList />
+      <ArticleList posts={articles} />
     </div>
   );
 }
--- src/components/ArticleList.jsx
+++ src/components/ArticleList.jsx
@@ -1,32 +1,13 @@
 import Article from "./Article.jsx"
 
-
- const articles = [
-        {
-            id: 1,
-            title: "My First Article",
-            date: "2024-06-01",
-            preview: "First article"
-        },
-
-        {
-            id: 2,
-            title: "My Second Article",
-            date: "2024-06-02",
-            preview: "Second article"
-        }
-        ]
-
-function ArticleList() {
-   
+function ArticleList({ posts }) {
     return (
         <main>
-                {articles.map(article => (
+            {posts.map(article => (
                 <Article key={article.id} title={article.title} date={article.date} preview={article.preview} />
             ))}
         </main>
     )
 }
 
-
-export default ArticleList
+export default ArticleList;
